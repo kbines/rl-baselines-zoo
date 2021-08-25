0802c712c2b929b1cd2019129374f0cb21098797
kbines <kbines@gmail.com>
2021-08-25 23:01:40 +0100

    all

---
+++ ac2_50_500_lstm.sh
@@ -0,0 +1,33 @@
+python train.py \
+ --algo a2c \
+ --env PortfolioAllocation-v0 \
+ -tb tensorboard \
+ --log-folder ac2_50_500_lstm_dr/log \
+ --n-timesteps 2517 \
+ --save-freq 10 \
+ --n-trials 5 \
+ --optimize \
+ --verbose 1 \
+ --env-kwargs sample_size:500  reward_function:"'daily_returns'"
+ python train.py \
+ --algo a2c \
+ --env PortfolioAllocation-v0 \
+ -tb tensorboard \
+ --log-folder ac2_50_500_lstm_pv/log \
+ --n-timesteps 2517 \
+ --save-freq 10 \
+ --n-trials 5 \
+ --optimize \
+ --verbose 1 \
+ --env-kwargs sample_size:500 reward_function:"'portfolio_value'"
+ python train.py \
+ --algo a2c \
+ --env PortfolioAllocation-v0 \
+ -tb tensorboard \
+ --log-folder ac2_50_500_lstm_sharpe/log \
+ --n-timesteps 2517 \
+ --save-freq 10 \
+ --n-trials 5 \
+ --optimize \
+ --verbose 1 \
+ --env-kwargs sample_size:500 reward_function:"'sharpe'"
