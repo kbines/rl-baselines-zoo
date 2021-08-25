ce5ae51e1f1f9119e01a05e02fda8d94fd5a1444
kbines <kbines@gmail.com>
2021-08-25 22:34:48 +0100

    tune

--- ac2_50_500_lstm_pv.bat
+++ ac2_50_500_lstm_pv.bat
@@ -2,10 +2,10 @@ python train.py^
  --algo a2c^
  --env PortfolioAllocation-v0^
  -tb tensorboard^
- --log-folder ac2_50_500_lstm_sharpe/log^
+ --log-folder ac2_50_500_lstm_pv/log^
  --n-timesteps 2517^
  --save-freq 10^
  --n-trials 5^
  --optimize^
  --verbose 1^
- --env-kwargs sample_size:500 reward_function:"'sharpe'"
+ --env-kwargs sample_size:500 reward_function:"'portfolio_value'"
---
+++ ac2_50_500_lstm_pv.sh
@@ -0,0 +1,11 @@
+python train.py \
+ --algo a2c \
+ --env PortfolioAllocation-v0 \
+ -tb tensorboard \
+ --log-folder ac2_50_500_lstm_pv/log \
+ --n-timesteps 2517 \
+ --save-freq 10 \
+ --n-trials 5 \
+ --optimize \
+ --verbose 1 \
+ --env-kwargs sample_size:500 reward_function:"'portfolio_value'"
